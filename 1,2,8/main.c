8ac93144b12a888b93d6a30229345451d922a579
KBorsuk <58912501+KBorsuk@users.noreply.github.com>
2020-02-26 10:54:07 +0100

    Add files via upload

---
+++ 1,2,8/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    scanf("%d %d %d", &a, &b, &c);
+    int x =(a+b+c)/3;
+    printf("%d", x);
+    return 0;
+}
