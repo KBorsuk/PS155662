0a24691c47bb444f0cba2f882a508e0cfbafc6e3
KBorsuk <58912501+KBorsuk@users.noreply.github.com>
2020-02-26 10:41:54 +0100

    Add files via upload

---
+++ 1,2,6/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    int b;
+    int c;
+    int d;
+    scanf("%d %d %d %d", &a, &b, &c, &d);
+    printf("%d \n",a);
+    printf("%d \n",b);
+    printf("%d \n",c);
+    printf("%d \n",d);
+    return 0;
+}
